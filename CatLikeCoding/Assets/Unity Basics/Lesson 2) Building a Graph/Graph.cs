585628bdd72330217ad4d300d4d6610af58d7138
AHamelGH <114834293+AHamelGH@users.noreply.github.com>
2023-05-24 22:39:35 -0500

    Unity Basics Lesson 4
    Implemented the code form lesson 4 of Unity Basics

--- CatLikeCoding/Assets/Unity Basics/Lesson 2) Building a Graph/Graph.cs
+++ CatLikeCoding/Assets/Unity Basics/Lesson 2) Building a Graph/Graph.cs
@@ -14,7 +14,20 @@ public class Graph : MonoBehaviour
     [SerializeField]
     FunctionLibrary.FunctionName function;
 
+    public enum TransitionMode { Cycle, Random}
+    [SerializeField]
+    TransitionMode transitionMode;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
     private Transform[] points;
+
+    private float duration;
+
+    private bool transitioning;
+
+    private FunctionLibrary.FunctionName transitionFunction;
 
     void Awake()
     {
@@ -35,6 +48,36 @@ public class Graph : MonoBehaviour
 
     void Update()
     {
+        duration +=  Time.deltaTime;
+
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+
+        if (transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateFunction();
+        }
+    }
+
+    private void UpdateFunction()
+    {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
 
         float time = Time.time;
@@ -51,12 +94,39 @@ public class Graph : MonoBehaviour
 
             float u = (x + 0.5f) * step -1f;
             points[i].localPosition = f(u, v, time);
-
-            /*Transform point = points[i];
-            Vector3 position = point.localPosition;
-
-            position.y = f(position.x, position.z, time);
-            point.localPosition = position;*/
         }
+    }
+
+    private void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+
+        float progress = duration / transitionDuration;
+
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step -1f;
+            }
+
+            float u = (x + 0.5f) * step -1f;
+
+            points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
+        }
+    }
+
+    private void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ? 
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
     }
 }
